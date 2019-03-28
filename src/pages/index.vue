ee140e74a0ba54e44eba7c66c05b632118cbce49
Eduardo P. Rivero <jrperedo@gmail.com>
2019-03-28 15:46:56 -0500

    feat(web): added two more demos

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -73,6 +73,16 @@
         text="My Style Guide"
         url="https://style-guide.eperedo.com/guides/js"
       />
+      <app-gallery-item
+        image="https://res.cloudinary.com/drukp4ipu/image/upload/v1553805893/portfolio/jardi.png"
+        text="Jardi - A income/expense app"
+        url="https://jardi.netlify.com"
+      />
+      <app-gallery-item
+        image="https://res.cloudinary.com/drukp4ipu/image/upload/v1553805956/portfolio/ama-sua.png"
+        text="Ama Sua"
+        url="http://amasua.netlify.com"
+      />
     </section>
   </section>
 </template>
